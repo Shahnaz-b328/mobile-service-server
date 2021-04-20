65c5e90f45e354c7b49248fcfefe69183e2810a1
Shahnaz-b328 <shahnazb722@gmail.com>
2021-04-20 20:20:09 +0600

    complete-website-server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-const port = 5000
+const port = 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tmexp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -104,4 +104,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port)
+app.listen(process.env.PORT || port)
